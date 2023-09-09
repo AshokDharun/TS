782b27858ea804487a4d658a21ea6c01a954a0fb
AshokDharun <125041008+AshokDharun@users.noreply.github.com>
2023-09-09 09:39:02 +0530

    Arrays

--- sandbox.ts
+++ sandbox.ts
@@ -17,3 +17,22 @@ const cir=(dima:number)=>
 return dima*Math.PI;
 }
 console.log(cir(7.5 ));
+
+//Arrays 
+let names=['asjok','dfs','difn'];
+//names.push(123);
+names.push("adef");
+names.push("thnk");
+console.log(names);
+names[0]="First";
+console.log(names);
+
+//array tupe declar as number
+let aray:number[]=[1,2,3,4,6];
+
+console.log(aray);
+//
+const mixed=['ashok',1,'233',"dhar",56];
+console.log(mixed);
+mixed.push(77);
+console.log(mixed[2]);
