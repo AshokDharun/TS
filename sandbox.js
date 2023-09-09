5d34da011c771abc9c020d0d232a329a00256b25
AshokDharun <125041008+AshokDharun@users.noreply.github.com>
2023-09-09 09:19:05 +0530

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,13 @@
+var character = 'like';
+var age = 30;
+var limit = true;
+var uname = 'ashok';
+uname = "dharub";
+age = 9;
+//character=0;
+//limit=9;\
+//Functions 
+var cir = function (dima) {
+    return dima * Math.PI;
+};
+console.log(cir(7.5));
